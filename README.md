690cb10d771d06234018dce2c0e7deb048c6fe77
Gerald Hurdle <hurdleg@algonquincollege.com>
2016-10-31 13:11:59 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+## URL Connection ##
+  Requesting content with HttpURLConnection.
+
+## Source Code ##
+
+  Available from GitHub:
+
+  https://github.com/hurdleg/URLConnection.git
+
+## Reference ##
+
+  Based on URLConnection in _Connecting Android Apps to RESTful Web Services_ with David Gassner
---
+++ app/src/main/java/com/algonquincollege/hurdleg/doorsopen/MainActivity.java
@@ -0,0 +1,108 @@
+package com.algonquincollege.hurdleg.doorsopen;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Requesting content with HttpURLConnection.
+ *
+ * @author Gerald.Hurdle@AlgonquinCollege.com
+ *
+ * Reference: based on URLConnection in "Connecting Android Apps to RESTful Web Services" with David Gassner
+ */
+public class MainActivity extends Activity {
+
+    // URL to my RESTful API Service hosted on my Bluemix account.
+    public static final String REST_URI = "https://planets-hurdleg.mybluemix.net/planets";
+
+    private TextView    output;
+    private ProgressBar pb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//		Initialize the TextView for vertical scrolling
+        output = (TextView) findViewById(R.id.textView);
+        output.setMovementMethod(new ScrollingMovementMethod());
+
+        pb = (ProgressBar) findViewById(R.id.progressBar1);
+        pb.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_get_data) {
+            if (isOnline()) {
+                requestData( REST_URI );
+            } else {
+                Toast.makeText(this, "Network isn't available", Toast.LENGTH_LONG).show();
+            }
+        }
+        return false;
+    }
+
+    private void requestData(String uri) {
+        MyTask task = new MyTask();
+        task.execute(uri);
+    }
+
+    protected void updateDisplay(String message) {
+        output.append(message + "\n");
+    }
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private class MyTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            updateDisplay("Starting task");
+            pb.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String content = HttpManager.getData(params[0]);
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            updateDisplay(result);
+            pb.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            updateDisplay(values[0]);
+        }
+    }
+}
